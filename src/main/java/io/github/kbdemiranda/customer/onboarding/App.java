2fe4fd131b11b035a60f26a96fbed555d9149926
Kaique de Miranda <kbd.miranda@gmail.com>
2026-05-04 16:22:05 -0300

    refactor: rename main application class and update datasource configuration

--- src/main/java/io/github/kbdemiranda/customer/onboarding/App.java
+++ src/main/java/io/github/kbdemiranda/customer/onboarding/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomerOnboardingApiApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(CustomerOnboardingApiApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 
 }
